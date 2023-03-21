e01598eccf7941c61ce36c95664765afeff56dbf
NygilNet <nygilnet@gmail.com>
2023-03-21 18:29:20 -0500

    first render deployment

--- app/api/notebook_routes.py
+++ app/api/notebook_routes.py
@@ -2,12 +2,3 @@ from flask import Blueprint, jsonify
 from app.models import db, Notebook
 
 notebook_routes = Blueprint('notebooks', __name__)
-
-@notebook_routes.get('')
-def notebooks():
-    """
-    Test route to query for all the notebooks in the db. NOT for production use.
-    """
-    print('DELETE THIS AT SOME POINT CONSOLE.LOG')
-    notebooks = Notebook.query.all()
-    return { 'notebooks': [notebook.to_dict() for notebook in notebooks] }
