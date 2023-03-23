4996e647a309de5c21b85d9c39742caf4ed94520
NygilNet <nygilnet@gmail.com>
2023-03-23 13:15:19 -0500

    LUNCH: working on creating note form

---
+++ react-app/src/components/CreateNote/index.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+function CreateNote() {
+
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const [saved, setSaved] = useState('All changes saved');
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        setSaved('Saving...');
+
+
+
+        setSaved('All changes saved');
+    }, [title, content])
+
+    return(
+        <>
+            <h1>hello from create a note</h1>
+            <form>
+                <input
+                type="text"
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+                maxLength="255"
+                placeholder="Title"
+                />
+                <ReactQuill theme="snow" value={content} onChange={setContent} />
+            </form>
+            <p>{saved}</p>
+        </>
+    )
+
+}
+
+export default CreateNote;
